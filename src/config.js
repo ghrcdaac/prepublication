b5e75668c2f99511e145b5ed5c7543d17cf257cf
Navaneeth R Selvaraj <navaneeth026@gmail.com>
2024-11-18 10:28:23 -0600

    upgrade version

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     cloudWatchUrlBase: 'https://list.ghrc.earthdata.nasa.gov/',
     sourceIMGUrl: 'https://data.ghrc.earthdata.nasa.gov/ghrcw-public/',
-    version: 'v1.1.0'
+    version: 'v1.1.1'
 }
 
 export default config
